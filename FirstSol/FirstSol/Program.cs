5e6fdd64aed51d147e8328c34a19ea9ad3f67d10
Andrii <demonictoast@gmail.com>
2015-04-18 23:51:30 +0300

    More changes!

--- FirstSol/FirstSol/Program.cs
+++ FirstSol/FirstSol/Program.cs
@@ -4,15 +4,21 @@ namespace FirstSol
 {
 	class SupClass
 	{
-		public void Method();
+		public void Method()
+		{
+			Console.WriteLine ("Method belongs To SupClass");
+		}
 	}
 	
 	class MainClass
 	{
 		public static void Main (string[] args)
 		{
-			
+			SupClass instance = new SupClass ();
+			instance.Method ();
+
 			 Console.WriteLine ("Hello World!");
+
 		}
 	}
 }
