1912a153e514892615d4a99be02d85534cf85fde
AndriiHaiduchenko <demonictoast@gmail.com>
2015-04-18 23:45:48 +0300

    Update Program.cs

--- FirstSol/FirstSol/Program.cs
+++ FirstSol/FirstSol/Program.cs
@@ -2,11 +2,17 @@
 
 namespace FirstSol
 {
+	class SupClass
+	{
+		public void Method();
+	}
+	
 	class MainClass
 	{
 		public static void Main (string[] args)
 		{
-			Console.WriteLine ("Hello World!");
+			
+			 Console.WriteLine ("Hello World!");
 		}
 	}
 }
